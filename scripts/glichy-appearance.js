7d71d212c2ea494b25b186f16d44d0c86bf60a12
Matej <hozlarmatej0@gmail.com>
2025-03-02 18:31:29 +0100

    about and project htmls

--- scripts/glichy-appearance.js
+++ scripts/glichy-appearance.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".pixelated").forEach(el => {
+        if(!el.classList.contains('dropdown-menu')){
         setTimeout(() => {
             el.classList.add("visible");
-        }, 500);
+        }, 500);}
     });
+
 });
--- scripts/translations.js
+++ scripts/translations.js
@@ -7,7 +7,25 @@ const translations = {
         'button-contact' : 'Contact',
         'homepage-title' : 'A Bit About Me',
         'homepage-text' : 'Hi, my name is Matej Hozlár, and I’m a young, passionate learner with a deep love for IT and problem-solving. Ever since I discovered the world of technology, I’ve been fascinated by how things work and how I can make them better. I started studying IT at university, but for now, my studies are on pause. However, that hasn’t stopped me from learning, growing, and improving every day. I’m always exploring new technologies, working on projects, and challenging myself to solve complex problems.',
-        'homepage-getintouch' : 'Get in touch'
+        'homepage-getintouch' : 'Get in touch',
+        'homepage-cv' : 'Download CV',
+        /* AboutMePage */
+        'aboutpage-title' : 'Passionate IT Enthusiast | Problem Solver',
+        'aboutpage-text' : 'Hi, my name is Matej Hozlár, and I’m a young, passionate learner with a deep love for IT and problem-solving. Ever since I discovered the world of technology, I’ve been fascinated by how things work and how I can make them better. I started studying IT at university, but for now, my studies are on pause. However, that hasn’t stopped me from learning, growing, and improving every day. I’m always exploring new technologies, working on projects, and challenging myself to solve complex problems. What drives me the most is the constant evolution of technology—there’s always something new to discover, and I thrive on that. Whether it’s coding, troubleshooting, or diving into new concepts, I love finding solutions and pushing myself forward. I believe that hard work, curiosity, and persistence are the keys to success, and I’m always looking for opportunities to expand my knowledge and skills. The journey in IT is never-ending, and I’m excited for what’s ahead!',
+        'aboutpage-workexp' : 'Work Experience',
+        'aboutpage-netdes' : "I was responsible for configuring and maintaining the company's IT networks.",
+        'aboutpage-cryptodes' : 'I am working on Crypto researches, that are used on sites like altfins.com,',
+        'aboutpage-edu' : 'Education',
+        'aboutpage-prg' : 'Prague',
+        'aboutpage-self' : 'Self-Taught Developer - Online Courses',
+        'aboutpage-selfp' : 'Projects',
+        'aboutpage-skills' : 'Skills',
+        'aboutpage-advanced' : 'Advanced',
+        'aboutpage-programming' : 'Programming',
+        'aboutpage-beginner' : 'Beginner',
+        'aboutpage-languages' : 'Languages',
+        'aboutpage-en' : 'English - Upper-itermediate (B2)',
+        'aboutpage-czsk' : 'Slovak & Czech - Expert (C2)'
     },
 
     'sk': {
@@ -18,7 +36,25 @@ const translations = {
         'button-contact' : 'Kontakt',
         'homepage-title' : 'Niečo málo o mne',
         'homepage-text' : 'Ahoj, volám sa Matej Hozlár a som mladý, vášnivý študent s hlbokou láskou k IT a riešeniu problémov. Odkedy som objavil svet technológií, fascinovalo ma, ako veci fungujú a ako ich môžem zlepšiť. Začal som študovať IT na univerzite, no momentálne mám štúdium pozastavené. To mi však nebráni v tom, aby som sa každý deň učil, rástol a zlepšoval. Neustále objavujem nové technológie, pracujem na projektoch a posúvam svoje hranice riešením náročných problémov.',
-        'homepage-getintouch' : 'Kontakt'
+        'homepage-getintouch' : 'Kontakt',
+        'homepage-cv' : 'Stiahnuť CV',
+        /* AboutMePage */
+        'aboutpage-title' : 'Vášnivý nadšenec IT | Riešiteľ problémov',
+        'aboutpage-text' : 'Ahoj, volám sa Matej Hozlár a som mladý, nadšený študent s hlbokou láskou k IT a riešeniu problémov. Odkedy som objavil svet technológií, fascinovalo ma, ako veci fungujú a ako ich môžem zlepšiť. Začal som študovať IT na univerzite, no momentálne mám štúdium pozastavené. To ma však neodradilo od učenia sa, rastu a zlepšovania sa každý deň. Neustále objavujem nové technológie, pracujem na projektoch a posúvam svoje limity pri riešení zložitých problémov. To, čo ma najviac poháňa, je neustály vývoj technológií – vždy je niečo nové na objavenie, a práve to ma motivuje. Či už ide o programovanie, hľadanie chýb alebo skúmanie nových konceptov, rád nachádzam riešenia a posúvam sa vpred. Verím, že tvrdá práca, zvedavosť a vytrvalosť sú kľúčom k úspechu, a preto vždy hľadám príležitosti, ako rozšíriť svoje vedomosti a zručnosti. Cesta v IT nikdy nekončí a ja sa teším na to, čo ma čaká!',
+        'aboutpage-workexp' : 'Pracovné Skúsenosti',
+        'aboutpage-netdes' : "Bol som zodpovedný za konfiguráciu a údržbu IT sietí spoločnosti.",
+        'aboutpage-cryptodes' : 'Pracujem na výskumoch v oblasti kryptomien, ktoré sa využívajú na weboch ako altfins.com,',
+        'aboutpage-edu' : 'Vzdelanie',
+        'aboutpage-prg' : 'Praha',
+        'aboutpage-self' : 'Samouk Vývojár - Online kurzy',
+        'aboutpage-selfp' : 'Projekty',
+        'aboutpage-skills' : 'Schopnosti',
+        'aboutpage-advanced' : 'Pokročilý',
+        'aboutpage-programming' : 'Programovacie Jazyky',
+        'aboutpage-beginner' : 'Začiatočník',
+        'aboutpage-languages' : 'Jazyky',
+        'aboutpage-en' : 'Angličtina - Stredne pokročilý (B2)',
+        'aboutpage-czsk' : 'Slovenčina a čeština - Expert (C2)'
     },
 
     'cz': {
@@ -29,7 +65,25 @@ const translations = {
         'button-contact' : 'Kontakt',
         'homepage-title' : 'Něco málo o mně',
         'homepage-text' : 'Ahoj, jmenuji se Matej Hozlár a jsem mladý, vášnivý student s hlubokou láskou k IT a řešení problémů. Od chvíle, kdy jsem objevil svět technologií, mě fascinovalo, jak věci fungují a jak je mohu vylepšit. Začal jsem studovat IT na univerzitě, ale momentálně mám studium pozastavené. To mi však nebrání v tom, abych se každý den učil, rozvíjel a zlepšoval. Neustále objevuji nové technologie, pracuji na projektech a posouvám své hranice řešením složitých problémů.',
-        'homepage-getintouch' : 'Kontakt'
+        'homepage-getintouch' : 'Kontakt',
+        'homepage-cv' : 'Stáhnout CV',
+        /* AboutMePage */
+        'aboutpage-title' : 'Vášnivý nadšenec IT | Řešitel problémů',
+        'aboutpage-text' : 'Ahoj, jmenuji se Matej Hozlár a jsem mladý, nadšený student s hlubokou láskou k IT a řešení problémů. Od chvíle, kdy jsem objevil svět technologií, mě fascinovalo, jak věci fungují a jak je mohu vylepšit. Začal jsem studovat IT na univerzitě, ale momentálně mám studium pozastavené. To mě však nezastavilo v učení, růstu a zlepšování se každý den. Neustále objevuji nové technologie, pracuji na projektech a posouvám své limity při řešení složitých problémů. To, co mě nejvíce pohání, je neustálý vývoj technologií – vždy je něco nového k objevování, a to mě motivuje. Ať už jde o programování, hledání chyb nebo studium nových konceptů, rád nacházím řešení a posouvám se dál. Věřím, že tvrdá práce, zvědavost a vytrvalost jsou klíčem k úspěchu, a proto vždy hledám příležitosti, jak rozšířit své znalosti a dovednosti. Cesta v IT nikdy nekončí, a já se těším na to, co mě čeká!',
+        'aboutpage-workexp' : 'Pracovní Zkušenosti',
+        'aboutpage-netdes' : "Byl jsem zodpovědný za konfiguraci a údržbu IT sítí společnosti.",
+        'aboutpage-cryptodes' : 'Pracuji na výzkumech v oblasti kryptoměn, které se využívají na webech jako altfins.com,',
+        'aboutpage-edu' : 'Vzdelání',
+        'aboutpage-prg' : 'Praha',
+        'aboutpage-self' : 'Samouk vývojář - Online kurzy',
+        'aboutpage-selfp' : 'Projekty',
+        'aboutpage-skills' : 'Schopnosti',
+        'aboutpage-advanced' : 'Pokročilý',
+        'aboutpage-programming' : 'Programovací jazyky',
+        'aboutpage-beginner' : 'Začátečník',
+        'aboutpage-languages' : 'Jazyky',
+        'aboutpage-en' : 'Angličtina - Středně pokročilý (B2)',
+        'aboutpage-czsk' : 'Slovenština a čeština - Expert (C2)'
     }
 
 }
